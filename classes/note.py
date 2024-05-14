19996cc2adc1cbb3976aecded6844b63b68883d9
ahmed151515 <ahmedarafa.200444@gmail.com>
2024-05-14 15:39:25 +0300

    add load & save in Todo & Note to handle json

--- classes/note.py
+++ classes/note.py
@@ -1,14 +1,30 @@
-import sqlalchemy as db
-from base import Base
+import json
 
 
-class Note(Base):
-
-    __tablename__ = "note"
-    id = db.Column("id", db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column("title", db.String(15))
-    text = db.Column("text", db.String(15))
+class Note():
 
     def __init__(self, title, text):
         self.title = title
         self.text = text
+
+    @classmethod
+    def load(cls):
+        with open("classes/note.json", "r")as f:
+            data = json.load(f)
+            result = []
+        for key, value in data.items():
+            result.append(Note(key, value["text"]))
+        return result
+
+    @classmethod
+    def save(cls, list):
+        result = {}
+
+        for task in list:
+            result[task.title] = {"text": task.text}
+
+            with open("classes/note.json", "w")as f:
+                json.dump(result, f, indent=4)
+
+    def __str__(self) -> str:
+        return f"{self.title}:{self.text}"
--- classes/todo.py
+++ classes/todo.py
@@ -1,14 +1,31 @@
-import sqlalchemy as db
-from base import Base
+import json
 
 
-class Todo(Base):
-
-    __tablename__ = "todo"
-    id = db.Column("id", db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column("title", db.String(15), nullable=False)
-    descraibe = db.Column("descraibe", db.String(50))
-
+class Todo():
     def __init__(self, title, descraibe):
         self.title = title
         self.descraibe = descraibe
+
+    @classmethod
+    def load(cls):
+        with open("classes/todo.json", "r")as f:
+            data = json.load(f)
+            result = []
+        for key, value in data.items():
+            result.append(Todo(key, value["descraibe"]))
+        return result
+
+    @classmethod
+    def save(cls, list):
+        result = {}
+
+        for task in list:
+            result[task.title] = {"descraibe": task.descraibe}
+
+            with open("classes/todo.json", "w")as f:
+                json.dump(result, f, indent=4)
+
+    def __str__(self) -> str:
+        return f"{self.title}:{self.descraibe}"
+
+
